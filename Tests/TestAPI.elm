54d521d575b11792b846c0ad2e52ce89b9e65c0d
bizmonger <scott.nimrod@bizmonger.net>
2017-05-30 08:42:22 -0400

    Added mock data for content for TestAPI

--- Tests/TestAPI.elm
+++ Tests/TestAPI.elm
@@ -1,6 +1,32 @@
 module Tests.TestAPI exposing (..)
 
 import Controls.Login as Login exposing (Model)
+import Domain.Core exposing (..)
+
+
+someUrl : Url
+someUrl =
+    Url "http://some_url.com"
+
+
+someTitle : Title
+someTitle =
+    Title "Some Title"
+
+
+submitter1 : Submitter
+submitter1 =
+    Submitter "Submitter 1"
+
+
+submitter2 : Submitter
+submitter2 =
+    Submitter "Submitter 2"
+
+
+submitter3 : Submitter
+submitter3 =
+    Submitter "Submitter 3"
 
 
 tryLogin : Login.Model -> Login.Model
@@ -13,3 +39,26 @@ tryLogin credentials =
             { username = credentials.username, password = credentials.password, loggedIn = True }
         else
             { username = credentials.username, password = credentials.password, loggedIn = False }
+
+
+recentSubmitters : List Submitter
+recentSubmitters =
+    [ submitter1
+    , submitter2
+    , submitter3
+    ]
+
+
+recentPodcasts : List Podcast
+recentPodcasts =
+    [ Podcast (Post submitter1 someTitle someUrl) ]
+
+
+recentArticles : List Article
+recentArticles =
+    [ Article (Post submitter2 someTitle someUrl) ]
+
+
+recentVideos : List Video
+recentVideos =
+    [ Video (Post submitter3 someTitle someUrl) ]
