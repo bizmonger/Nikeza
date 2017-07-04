c86adeed0a6baf14ccdca58d321a17c6697ae590
bizmonger <scott.nimrod@bizmonger.net>
2017-07-04 11:40:11 -0400

    Added test: "search yields profile"

--- Client/app/Tests/HelloTest.elm
+++ Client/app/Tests/HelloTest.elm
@@ -1,14 +1,17 @@
 module HelloTest exposing (..)
 
 import Controls.Login as Login exposing (Model)
+import Tests.TestAPI exposing (..)
+import Home exposing (..)
 import Settings exposing (..)
+import Navigation exposing (..)
 import Test exposing (..)
 import Expect
 
 
 suite : Test
 suite =
-    describe "Login module"
+    describe "My Tests"
         [ test "runtime.tryLogin succeeds with valid credentials" <|
             \_ ->
                 let
@@ -29,4 +32,23 @@ suite =
                         runtime.tryLogin login
                 in
                     Expect.equal result.loggedIn False
+        , test "search yields profile" <|
+            \_ ->
+                let
+                    location =
+                        Navigation.Location "" "" "" "" "" "" "" "" "" "" ""
+
+                    ( model, _ ) =
+                        Home.init location
+
+                    ( newState, _ ) =
+                        model |> Home.update (Search "Scott")
+
+                    onlyOne =
+                        (newState.contentProviders |> List.length) == 1
+
+                    isContentProvider1 =
+                        newState.contentProviders |> List.member contentProvider1
+                in
+                    Expect.equal (onlyOne && isContentProvider1) True
         ]
