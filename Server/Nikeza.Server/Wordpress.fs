a0b09076eb39732ce0df20c262f868bc626eac0c
yelluw <pr@yelluw.com>
2017-07-09 00:53:33 -0400

    Wordpress integration.

---
+++ Server/Nikeza.Server/Wordpress.fs
@@ -0,0 +1,18 @@
+module Nikeza.Server.Wordpress
+
+open System
+open System.Text
+open System.Net.Http
+open Nikeza.Wordpress.Rss.Data
+open Newtonsoft.Json
+
+let jsonRssFeed feedUrl = async { 
+        let client = new  HttpClient()
+        let! response = client.GetByteArrayAsync(feedUrl)                         
+                        |> Async.AwaitTask                                                     
+        let content = Encoding.UTF8.GetString(response,0, (response.Length - 1))                 
+       
+        let feed = deserializeXml content 
+        let json = JsonConvert.SerializeObject(feed)
+        return json
+}
