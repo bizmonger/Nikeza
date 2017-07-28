75fc705c19172e0f3d23efa10a00d0470c4fb2a1
Ody Mbegbu <odytrice@gmail.com>
2017-07-28 07:11:25 +0100

    Started work on Data Access Layer

---
+++ Server/Nikeza.Server/DB.fs
@@ -0,0 +1,30 @@
+module Nikeza.Server.DB
+
+open Nikeza.Server.Models
+open System
+open System.Data.SqlClient
+
+let findUser (connString: string) email passwordHash =
+    let query = "SELECT * FROM Profile Where Email = @email AND PasswordHash = @hash"
+    use connection = new SqlConnection(connString)
+
+    use command = new SqlCommand(query,connection)
+    command.Parameters.AddWithValue("@email", email) |> ignore
+    command.Parameters.AddWithValue("@hash", passwordHash) |> ignore
+
+    connection.Open()
+    let reader = command.ExecuteReader()
+    let profiles = 
+        seq {
+            while reader.Read() do 
+                yield { 
+                    ProfileId = reader.["Id"].ToString() |> int
+                    FirstName = reader.["FirstName"].ToString()
+                    LastName = reader.["LastName"].ToString()
+                    Email = reader.["Email"].ToString()
+                    ImageUrl = reader.["ImageUrl"].ToString()
+                    Bio = reader.["Bio"].ToString()
+                    Created = DateTime.Parse(reader.["Created"].ToString())
+                } 
+        }
+    profiles |> Seq.tryHead
