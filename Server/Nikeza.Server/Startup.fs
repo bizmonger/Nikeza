bdd2406f517eeae7c55b73c0ebb3d7782eb560fa
bizmonger <scott.nimrod@bizmonger.net>
2017-10-13 05:55:08 -0400

    Updated: builder.WithOrigins("*")

--- Server/Nikeza.Server/Startup.fs
+++ Server/Nikeza.Server/Startup.fs
@@ -38,7 +38,7 @@ let cookieAuth =
     )
 
 let configureCors (builder : CorsPolicyBuilder) =
-    builder.WithOrigins("http://localhost:8000").AllowAnyMethod().AllowAnyHeader() |> ignore
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader() |> ignore
 
 let configureApp (app : IApplicationBuilder) =
     app.UseCors configureCors |> ignore
