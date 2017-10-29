5b2a7a42c6d5cf5e117f07f3fb118549fe92b3cf
bizmonger <scott.nimrod@bizmonger.net>
2017-10-29 09:57:17 -0400

    Added partial support for StackOverflow

--- Server/Nikeza.Server/StackOverflow.fs
+++ Server/Nikeza.Server/StackOverflow.fs
@@ -1,8 +1,11 @@
 module Nikeza.Server.StackOverflow
-// Get most popular tags (100 / page)
+(* Get most popular tags (100 / page) *)
 // https://api.stackexchange.com/docs/tags#page=5&pagesize=100&order=desc&sort=popular&filter=!-.G.68grSaJm&site=stackoverflow&run=true
 
-// Answers
+(* Answers *)
 // https://api.stackexchange.com/2.2/users/492701/answers?order=desc&sort=activity&site=stackoverflow&filter=!Fcazzsr2b3M)LbUjGAu-Fs0Wf8
+
+(* Related Tags *)
+// https://stackoverflow.com/filter/tags?q=xamarin
 
 let getTags pages = []
