3d00ad9ecebae6c527af0b72936a1143b6cf325e
bizmonger <scott.nimrod@bizmonger.net>
2017-11-03 12:25:45 -0400

    Finished Medium.com JSON Parser

--- Server/Nikeza.Test/IntegrationTests.fs
+++ Server/Nikeza.Test/IntegrationTests.fs
@@ -35,27 +35,67 @@ let ``Parse Medium JSON`` () =
         postBlock
 
     let createLink (postBlock:string) =
-        let getTag (text:string) =
-            let tagsIndex =  text.IndexOf("\"tags\": [")
-            let tagsBlock1 = text.Substring(tagsIndex, text.Length - tagsIndex)
-            let startIndex = tagsBlock1.IndexOf('{')
-            let endIndex =   tagsBlock1.IndexOf('}')
-            let tagBlock =   tagsBlock1.Substring(startIndex, endIndex)
-            let tagParts =   tagBlock.Split('\n')
-            let tag =        parseValue(tagParts.[2])
-            tag
+
+        let getTagsBlock (postBlock:string) =
+            let startIndex = postBlock.IndexOf("\"tags\": [")
+            let block = postBlock.Substring(startIndex, postBlock.Length - startIndex)
+            let endIndex = block.IndexOf("],")
+            let tagsBlock = block.Substring(0, endIndex)
+            tagsBlock
+
+        let getTagBlock (tagsBlock:string) =
+            let startIndex = tagsBlock.IndexOf('{')
+
+            if startIndex >= 0
+                then let endIndex =   tagsBlock.IndexOf('}')
+                     let tagBlock =   tagsBlock.Substring(startIndex, endIndex)
+                     Some tagBlock
+                else None
+
+        let rec getTags (postBlock:string) (tags: (string option) list) =
+
+            let rec getTag (postBlock:string) : string option =
+                let tagBlock =   postBlock |> getTagsBlock |> getTagBlock
+
+                match tagBlock with
+                    | None       -> None
+                    | Some block ->
+                        if block.Contains("\"slug\":")
+                            then let tagParts = block.Split('\n')
+                                 let tag =      parseValue(tagParts.[2])
+                                 tag
+                            else let truncated = postBlock.Replace(block, "")
+                                 getTag truncated
+            
+            let tag =      getTag postBlock
+            let tagBlock = postBlock |> getTagsBlock |> getTagBlock
+
+            match tagBlock with
+            | None       -> tags
+            | Some block ->
+                if block.Contains("\"slug\":")
+                    then let truncated = postBlock.Replace(block, "")
+                         tag::tags |> getTags truncated
+                    else let nextBlock = postBlock |> getTagsBlock |> getTagBlock
+                         match nextBlock with
+                         | Some t -> 
+                            let truncated = postBlock.Replace(t, "")
+                            tag::tags |> getTags truncated
+                         | None   -> tag::tags
 
         let postParts = postBlock.Split("\n")
-        let topicResult = match getTag postBlock with
-                          | Some tag -> Some { Id= -1; Name=tag }
-                          | None     -> None
 
+        let topics = [] |> getTags postBlock 
+                        |> List.choose(fun tag -> match tag with
+                                                  | Some t -> Some { Id= -1; Name=t; }
+                                                  | None   -> None
+                                      )
         { Id= -1
           ProfileId=   "to be derived..."
           Title=        parseValue(postParts.[5]) |> function | Some title -> title | None -> ""
           Description=  ""
           Url=          parseValue(postParts.[1]) |> function | Some title -> "{0}" + title | None -> ""
-          Topics=       match topicResult with Some t -> [t] | None -> []
+          Topics=       topics |>List.toSeq |> Seq.distinct |> Seq.toList
           ContentType= "Article"
           IsFeatured=   false
         }
@@ -75,15 +115,11 @@ let ``Parse Medium JSON`` () =
         let truncatedText1 = text.Replace(removeTagBlock, "")
         let truncatedText2 = truncatedText1.Replace(postBlock, "")
 
-        let nextTagIndex = truncatedText2.IndexOf("\"homeCollectionId\":")
-        let nextPost = remainingText nextTagIndex truncatedText2
+        let nextPostIndex = truncatedText2.IndexOf("\"homeCollectionId\":")
+        let nextPost = remainingText nextPostIndex truncatedText2
         nextPost
 
-    let text =        File.ReadAllText(@"C:\Nikeza\Medium_json_examle.txt")
-    let postsIndex =  text.IndexOf("\"Post\": {") + 11
-    let partial =     text.Substring(postsIndex, text.Length - postsIndex)
-
-    let rec getLinks (partial:string) links =
+    let rec getLinks (partial:string) (originalContent:string) links =
 
         let identifier =   "\"homeCollectionId\":"
         let nextTagIndex =  partial.IndexOf(identifier)
@@ -92,16 +128,23 @@ let ``Parse Medium JSON`` () =
             then let nextPost = remainingText nextTagIndex partial
 
                  if nextPost.Contains(identifier)
-                    then let link =      createLink partial
-                         let postBlock = getPostBlock text
-                         let content =   getNextPost nextPost postBlock
-                         getLinks content (List.append links [link])
+                    then let endIndex =        partial.IndexOf("\"homeCollectionId\":", partial.IndexOf("\"homeCollectionId\":") + 1)
+                         let entirePostBlock = partial.Substring(0, endIndex - 300)
+                         let link =            createLink entirePostBlock
+                         let postBlock =       getPostBlock originalContent
+                         let content =         getNextPost nextPost postBlock
+                         
+                         [link] |> List.append links 
+                                |> getLinks content originalContent
 
                     else let link = createLink partial
                          List.append links [link]
             else links
 
-    let links = [] |> getLinks partial
+    let text =        File.ReadAllText(@"C:\Nikeza\Medium_json_examle.txt")
+    let postsIndex =  text.IndexOf("\"Post\": {") + 11
+    let postsBlock =  text.Substring(postsIndex, text.Length - postsIndex)
+    let links =       [] |> getLinks postsBlock text
     let y = links
     
     links |> List.length |> should (be greaterThan) 0
