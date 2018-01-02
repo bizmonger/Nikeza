5b6017ca5d80c438c8325799677c0e7887c6678d
bizmonger <scott.nimrod@bizmonger.net>
2018-01-02 13:28:32 -0500

    Asigned  member Latest to latest value

--- Mobile/Nikeza.Mobile/UILogic/Portal.recent.viewmodel.fs
+++ Mobile/Nikeza.Mobile/UILogic/Portal.recent.viewmodel.fs
@@ -11,8 +11,8 @@ type ViewModel(user:Provider) as x =
     let eventOccurred = new Event<_>()
     let mutable latest:Provider list = []
 
-    member x.User = user
-    member x.Latest = []
+    member x.User =   user
+    member x.Latest = latest
 
     member x.Load() =
         Query.latest <| ProfileId user.Profile.Id
