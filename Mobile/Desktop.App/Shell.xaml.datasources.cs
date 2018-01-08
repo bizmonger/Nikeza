40cf61185bb1c58d14c37039c872e3220f044474
bizmonger <scott.nimrod@bizmonger.net>
2018-01-08 18:57:27 -0500

    Partitioned shell navigation files

---
+++ Mobile/Desktop.App/Shell.xaml.datasources.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+using static Nikeza.Common;
+
+namespace Desktop.App
+{
+    public partial class Shell
+    {
+        static void ToDataSources(Frame AppFrame, ProfileRequest profile) =>
+            AppFrame.Navigate(new DataSourcesPage());
+    }
+}
---
+++ Mobile/Desktop.App/Shell.xaml.profileeditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Controls;
+using static Desktop.App.FunctionFactory;
+using static Nikeza.Mobile.Profile.Events;
+using ProfileEditorViewmodel = Nikeza.Mobile.UILogic.Portal.ProfileEditor.ViewModel;
+
+namespace Desktop.App
+{
+    public partial class Shell
+    {
+        static void FromPortalEditor(Frame AppFrame, ProfileEditorEvent theEvent)
+        {
+            if (theEvent.IsProfileSaved)
+                ToDataSources(AppFrame, theEvent.TryGetProfile().Value);
+
+            else if (theEvent.IsProfileSaveFailed)
+                ToError(AppFrame, theEvent);
+        }
+
+        static void ToProfileEditor(Frame AppFrame, RegistrationSubmissionEvent theEvent)
+        {
+            var portalPage = new PortalPage();
+            var viewmodel = new ProfileEditorViewmodel(theEvent.TryGetProfile().Value, SaveProfile());
+
+            viewmodel.EventOccurred += (s, e) => FromPortalEditor(AppFrame, e);
+            portalPage.DataContext = viewmodel;
+
+            AppFrame.Navigate(portalPage);
+        }
+
+        static void ToError(Frame AppFrame, ProfileEditorEvent theEvent) =>
+            AppFrame.Navigate(new ErrorPage());
+    }
+}
