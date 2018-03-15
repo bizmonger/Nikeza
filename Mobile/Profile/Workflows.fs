05650934165ce5bae14207cc12c9bb92986601e2
bizmonger <scott.nimrod@bizmonger.net>
2018-03-15 11:49:55 -0400

    Formatting updates

--- Mobile/Profile/Workflows.fs
+++ Mobile/Profile/Workflows.fs
@@ -12,44 +12,40 @@ module SubmitRegistration =
     type private SubmitWorkflow = SubmitFn -> Command -> RegistrationSubmissionEvent list
 
     let workflow : SubmitWorkflow =
-        fun submitFn command -> command |> function
-            Command.Execute form -> 
-                            form |> submitFn
-                                 |> ResultOf.Submit.Executed
-                                 |> Are.Registration.Submission.events
+        fun submitFn -> function
+        Command.Execute form -> 
+                        form |> submitFn
+                             |> ResultOf.Submit.Executed
+                             |> Are.Registration.Submission.events
 
 module ValidateRegistration =
     open Registration.Validate
 
     type private ValidateWorkflow = Validate -> RegistrationValidationEvent list
 
-    let workflow : ValidateWorkflow =
-        fun command -> command |> function
-            Validate.Execute form -> 
-                             form |> Registration.validate
-                                  |> ResultOf.Validate.Executed
-                                  |> Are.Registration.Validation.events
+    let workflow : ValidateWorkflow = function
+        Validate.Execute form -> 
+                         form |> Registration.validate
+                              |> ResultOf.Validate.Executed
+                              |> Are.Registration.Validation.events
 
 module Login =
     type private SessionWorkflow = LoginCommand -> LoginEvent list
 
-    let workflow : SessionWorkflow = 
-        fun command -> 
-            command |> function
-            Login credentials -> 
-                  credentials |> Try.login
-                              |> ResultOf.Login
-                              |> Are.Login.events
+    let workflow : SessionWorkflow = function
+        Login credentials -> 
+              credentials |> Try.login
+                          |> ResultOf.Login
+                          |> Are.Login.events
 
 module Logout =
     type private LogoutWorkflow = LogoutCommand -> LogoutEvent list
 
-    let workflow : LogoutWorkflow = 
-        fun command -> command |> function
-            Logout p ->
-                   p |> Try.logout
-                     |> ResultOf.Logout
-                     |> Are.Logout.events
+    let workflow : LogoutWorkflow = function
+        Logout p ->
+               p |> Try.logout
+                   |> ResultOf.Logout
+                   |> Are.Logout.events
 
 module Editor =
 
@@ -59,13 +55,11 @@ module Editor =
         
         type private ValidateWorkflow = ValidateCommand -> ProfileValidateEvent list
 
-        let workflow : ValidateWorkflow = 
-            fun command ->
-                command |> function
-                ValidateCommand.Execute form -> 
-                                        form |> Editor.validate 
-                                             |> ResultOf.Editor.Validate
-                                             |> Are.Editor.Validate.events
+        let workflow : ValidateWorkflow = function
+            ValidateCommand.Execute form -> 
+                                    form |> Editor.validate 
+                                            |> ResultOf.Editor.Validate
+                                            |> Are.Editor.Validate.events
 
     module Save =
         open Commands.ProfileEditor.Save
@@ -74,11 +68,11 @@ module Editor =
         type private SaveWorkflow = SaveProfileFn -> SaveCommand -> ProfileSaveEvent list
 
         let workflow : SaveWorkflow = 
-            fun saveFn command -> command |> function
-             SaveCommand.Execute profile -> 
-                                 profile |> saveFn
-                                         |> ResultOf.Editor.Save
-                                         |> Are.Editor.Save.events
+            fun saveFn -> function
+            SaveCommand.Execute profile -> 
+                                profile |> saveFn
+                                        |> ResultOf.Editor.Save
+                                        |> Are.Editor.Save.events
 
 module DataSources =
 
@@ -90,7 +84,7 @@ module DataSources =
 
         let workflow : SaveWorkflow = 
             fun savefn -> function
-                SaveCommand.Execute sources -> 
-                                    sources |> savefn
-                                            |> ResultOf.Save.Execute
-                                            |> Are.DataSources.Save.events    
+            SaveCommand.Execute sources -> 
+                                sources |> savefn
+                                        |> ResultOf.Save.Execute
+                                        |> Are.DataSources.Save.events    
