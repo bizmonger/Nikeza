a6f1d4aca86c81676cbfd2a80b27ff1fed45b2be
bizmonger <scott.nimrod@bizmonger.net>
2018-03-21 07:52:31 -0400

    Updated imports

--- Mobile/AppLogic/HandleRegistration.fs
+++ Mobile/AppLogic/HandleRegistration.fs
@@ -4,12 +4,11 @@ module RegistrationEvents =
 
     open System.Diagnostics
     open Nikeza.Mobile.Access.Events
+    open Nikeza.Mobile.UILogic.Registration
     open Design.Access
 
     module Register =
-
-        open Nikeza.Mobile.UILogic.Registration
-
+    
         let appendNavigation : Registration.SideEffects =
 
             fun app sideEffects ->
