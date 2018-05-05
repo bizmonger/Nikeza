0437b4d60ef4decdfaa3ee3fcd66e7805af9040c
bizmonger <scott.nimrod@bizmonger.net>
2018-05-05 14:12:01 -0400

    Portal page updates

--- Mobile/UILogic/TestAPI.fs
+++ Mobile/UILogic/TestAPI.fs
@@ -9,23 +9,18 @@ open System
 
 let someProfile = {
     Nikeza.Common.ProfileRequest.Id =        "0"
-    Nikeza.Common.ProfileRequest.FirstName = "first_name"
-    Nikeza.Common.ProfileRequest.LastName =  "last_name"
-    Nikeza.Common.ProfileRequest.Email =     "test@abc.com"
+    Nikeza.Common.ProfileRequest.FirstName = "Brian"
+    Nikeza.Common.ProfileRequest.LastName =  "Nimrod"
+    Nikeza.Common.ProfileRequest.Email =     "brian@abc.com"
     Nikeza.Common.ProfileRequest.Bio =       ""
     Nikeza.Common.ProfileRequest.ImageUrl =  "http://www.ngu.edu/myimages/silhouette2230.jpg"
     Nikeza.Common.ProfileRequest.Sources =   []
 }
 
-let someProfileRequest = {
-    Nikeza.Common.ProfileRequest.Id =        "0"
-    Nikeza.Common.ProfileRequest.FirstName = "first_name"
-    Nikeza.Common.ProfileRequest.LastName =  "last_name"
-    Nikeza.Common.ProfileRequest.Email =     "test@abc.com"
-    Nikeza.Common.ProfileRequest.Bio =       ""
-    Nikeza.Common.ProfileRequest.ImageUrl =  "http://www.ngu.edu/myimages/silhouette2230.jpg"
-    Nikeza.Common.ProfileRequest.Sources =   []
-}
+let someProfile2 = {someProfile with FirstName="Cherice"; LastName="Johnson"}
+let someProfile3 = {someProfile with FirstName="Joel";    LastName="Roach"}
+
+let someProfileRequest = someProfile
 
 let somePortfolio = { 
     Answers  = []
@@ -34,10 +29,10 @@ let somePortfolio = {
     Podcasts = []
 }
 
-let someLink = { 
+let someLink1 = { 
     Id=           123
     ProfileId=   "profileId1"
-    Title=       "Some Link Title"
+    Title=       "Some Link Title_1"
     Description= "Some link description"
     Url=         "http://some_url.com"
     Topics=       []
@@ -46,14 +41,20 @@ let someLink = {
     Timestamp=    DateTime.Now
 }
 
+let someLink2 = {someLink1 with Title="Some Link Title_2"}
+let someLink3 = {someLink1 with Title="Some Link Title_3"}
+
 let someProvider = {
     Nikeza.Common.ProviderRequest.Profile =      someProfile
     Nikeza.Common.ProviderRequest.Topics =        []
     Nikeza.Common.ProviderRequest.Portfolio=     somePortfolio
-    Nikeza.Common.ProviderRequest.RecentLinks=   [someLink;someLink;someLink]
+    Nikeza.Common.ProviderRequest.RecentLinks=   [someLink1;someLink2;someLink3]
     Nikeza.Common.ProviderRequest.Subscriptions= []
     Nikeza.Common.ProviderRequest.Followers=     []
 }
+
+let someProvider2 = {someProvider with Profile=someProfile2}
+let someProvider3 = {someProvider with Profile=someProfile3}
 
 open Nikeza.Mobile.Access
 
@@ -107,10 +108,10 @@ let mockPlatforms : PlatformsFn =
                 ]
 
 let mockRecent : RecentFn =
-    fun _ -> Ok [someProvider;someProvider;someProvider]
+    fun _ -> Ok [someProvider;someProvider2;someProvider3]
 
 let mockSubscriptions : SubscriptionsFn =
-    fun _ -> Ok [someProvider;someProvider;someProvider]
+    fun _ -> Ok [someProvider;someProvider2;someProvider3]
 
 let mockPortfolio : PortfolioFn =
     fun _ -> Ok someProvider
