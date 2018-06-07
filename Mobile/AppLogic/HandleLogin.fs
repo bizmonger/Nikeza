19b1c77f645eeac9aa4480f67ad88e23e9f0270c
bizmonger <scott.nimrod@bizmonger.net>
2018-06-07 08:09:37 -0400

    Removed appendNavigation

--- Mobile/AppLogic/HandleLogin.fs
+++ Mobile/AppLogic/HandleLogin.fs
@@ -5,45 +5,31 @@ open Nikeza.Common
 open Nikeza.Mobile.UILogic
 open Nikeza.Access.Specification.Login
 open Nikeza.Access.Specification.Events
-open Nikeza.Mobile.AppLogic.Specification.Access
 open Navigation
 open PageFactory
 
-module LoginEvents =
-    
-    let appendNavigation : Login.AddSideEffects =
-
-        fun app sideEffects ->
-
-            let handle = function
-                | LoggedIn        provider    -> app |> navigate (portalPage <| provider.Profile) provider
-                | FailedToConnect credentials -> app |> navigate errorPage credentials.Email
-
-                | FailedToAuthenticate _ -> ()
-
-            let forLoginAttempt = sideEffects.ForLoginAttempt
-            let handlers = { Head= forLoginAttempt.Head
-                             Tail= handle::forLoginAttempt.Tail 
-                           }
-
-            { sideEffects with ForLoginAttempt= handlers }
 
 module Login =
 
-    open LoginEvents
     open System.Diagnostics
 
     let dependencies =
 
-        let log = function
+        let log' = function
             | LoggedIn             user        -> Debug.WriteLine(sprintf "Login successful:\n %A" user)
             | FailedToConnect      credentials -> Debug.WriteLine(sprintf "Error: Unable to connect to server:\n %A" credentials)
             | FailedToAuthenticate credentials -> Debug.WriteLine(sprintf "Warning: Unable to authenticate user:\n %A" credentials)
 
-        let handlers =    { Head=log; Tail=[] }
-        let sideEffects = { ForLoginAttempt= handlers } |> appendNavigation Application.Current
+        let navigate' = function
+            | LoggedIn        provider    -> Application.Current |> navigate (portalPage provider.Profile) provider
+            | FailedToConnect credentials -> Application.Current |> navigate errorPage credentials.Email
+
+            | FailedToAuthenticate _ -> ()
+
+        let handlers =    { Head=log'; Tail=[navigate'] }
+        let sideEffects = { ForLoginAttempt= handlers }
         let attempt =     { Login= TestAPI.mockLogin }
     
-        { SideEffects=    sideEffects; 
-          Attempt= attempt 
+        { SideEffects= sideEffects; 
+          Attempt=     attempt 
         }
--- Mobile/AppLogic/HandleNavigation.fs
+++ Mobile/AppLogic/HandleNavigation.fs
@@ -5,10 +5,8 @@ open System.Diagnostics
 
 let navigate page context (app:Application) =
 
-    try 
-        let expression = sprintf "\n\n** Request: Navigate to %s\n\n** Payload as follows:\n\n %A"
-        Debug.WriteLine <| expression (page.ToString()) context
+    let expression = sprintf "\n\n** Request: Navigate to %s\n\n** Payload as follows:\n\n %A"
+    Debug.WriteLine <| expression (page.ToString()) context
 
-        app.MainPage <- page
-
+    try app.MainPage <- page
     with ex -> Debug.WriteLine(ex.Message); raise ex
