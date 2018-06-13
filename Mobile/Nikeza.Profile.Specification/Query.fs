64746611913df008c1205bb11a9fc84f06f9a693
bizmonger <scott.nimrod@bizmonger.net>
2018-06-13 07:46:37 -0400

    Combined Profile and Subscriptions specifications into Portal specification

---
+++ Mobile/Nikeza.Profile.Specification/Query.fs
@@ -0,0 +1,9 @@
+﻿namespace Nikeza.Portal.Specification
+
+open Nikeza.Common
+
+module Query =
+   
+    type RecentFn =        ProfileId -> Result<ProviderRequest list, ProfileId error>
+    type SubscriptionsFn = ProfileId -> Result<ProviderRequest list, ProfileId error>
+    type MembersFn =       unit      -> Result<ProviderRequest list, ProfileId error>
---
+++ Mobile/Nikeza.UI.Specification/Pages.fs
@@ -0,0 +1,29 @@
+module Nikeza.UI.Specification.Pages
+
+open Nikeza.Common
+open Nikeza.DataTransfer
+open Nikeza.Access.Specification
+
+type PageRequested =
+    | Registration
+    | RegistrationError of ValidatedForm error
+
+    | Portal            of Profile
+    | EditProfile       of UnvalidatedForm
+                        
+    | Latest            of Profile
+    | Subscriptions     of Profile
+    | Followers         of Profile
+    | Members
+                        
+    | Portfolio         of Profile
+    | PortfolioError    of ProviderId error
+
+    | Videos            of ProviderId
+    | Articles          of ProviderId
+    | Answers           of ProviderId
+    | Podcasts          of ProviderId
+                        
+    | TopicLinks        of Profile
+
+    | Error             of string error
